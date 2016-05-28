31f41a9add78ff2fe939b4c73d7077f232aba4ca
Matt Sharp <matt.sharp@hotmail.com>
2016-05-28 15:36:31 +0100

    Added gulpfile with connect server and live reload

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+
+//specify required gulp dependencies
+var gulp = require('gulp'),
+  connect = require('gulp-connect');
+ 
+//define gulp server task with livereload as true
+gulp.task('connect', function() {
+  connect.server({
+    root: '',
+    livereload: true
+  });
+});
+ 
+//specify reload task - when called tell connect server to reload
+gulp.task('reload', function () {
+  gulp.src('*.html')
+    .pipe(connect.reload());
+});
+ 
+//specify files to watch, and then call reload function on change
+gulp.task('watch', function () {
+  gulp.watch(['*.html', 'app/*.js', 'app/*/*/*.js'], ['reload']);
+});
+ 
+//gulp default will run connect and watch
+gulp.task('default', ['connect', 'watch']);
